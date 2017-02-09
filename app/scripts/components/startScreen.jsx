3993472632f95e85dcd31b8d0cf8cd50231bf4b2
Luis Rene Onate <lonate12@gmail.com>
2017-02-09 13:41:32 -0800

    Rough animation for opening screen complete.

--- app/scripts/components/startScreen.jsx
+++ app/scripts/components/startScreen.jsx
@@ -1,12 +1,34 @@
 var React = require('react');
 
 var StartScreen = React.createClass({
+  getInitialState: function(){
+    return{
+      loadCompleted: false
+    }
+  },
+  zoomIn: function(){
+    var max = (screen.width * .6) + 'px';
+    var self = this;
+
+    document.getElementById('logo').addEventListener("transitionend", function(){
+      self.setState({loadCompleted: true});
+      console.log('fired');
+    });
+
+    setTimeout(function(){
+      document.getElementById('logo').style.width = max;
+    });
+  },
   render: function(){
     return(
       <div className="row fs-container">
-        <img src="./images/star-wars-logo.svg"></img>
+        <img  id="logo" src="./images/star-wars-logo.svg" className="center-block"></img>
+        <button className={this.state.loadCompleted ? "start-button center-block" : "hide"}>Click here to begin!!!</button>
       </div>
     );
+  },
+  componentDidMount: function(){
+    this.zoomIn();
   }
 });
 
