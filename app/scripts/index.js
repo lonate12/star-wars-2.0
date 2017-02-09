d2167cb05831452003bdffe740f09817e721428f
Luis Rene Onate <lonate12@gmail.com>
2017-02-08 23:00:42 -0800

    Added some promise work.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,10 +3,16 @@ var Person = require('./models/Person.js').Person;
 var Planet = require('./models/Planet.js').Planet;
 var $ = require('jquery');
 
-var randomPlanet = new Planet({'planetNumber': 4});
+var randomPlanet = new Planet({'planetNumber': 1});
 
 randomPlanet.fetch().then(function(){
-  randomPlanet.loadHints();
+  // randomPlanet.loadHints();
+  console.log(randomPlanet);
+  Promise.all(requests()).then(function(){
+    console.log('yep');
+  }, function(){
+    console.log('nope');
+  });
 });
 
 $('#button').on('click', function(){
@@ -14,3 +20,13 @@ $('#button').on('click', function(){
   console.log(randomPlanet.hint2());
   console.log(randomPlanet.hint3());
 });
+
+function requests(){
+  var requestsArray = [];
+
+  randomPlanet.get('residents').forEach(function(url){
+    requestsArray.push($.ajax(url));
+  });
+
+  return requestsArray;
+};
